edc999934e6f60fc8eb44ff30926b025999311a5
zia-ur-rehman-khan <03112397853zia@gmail.com>
2021-07-10 07:26:22 +0500

    working on add jobs

--- module/job/jobRoute.js
+++ module/job/jobRoute.js
@@ -5,7 +5,7 @@ const jobControler = require("./jobController")
 
 
 router.post("/add-new-job", jobControler.addNewJob)
-router.get("/get-all-jobs", jobControler.getAllJob)
+// router.get("/get-all-jobs", jobControler.getAllJob)
 router.post("/user-apply-job", jobControler.userApplyInJob)
 
 module.exports = router
